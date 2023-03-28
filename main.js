55a49108c978d2803fd8e18ff3c4726d93210119
Luan Oliveira <125591154+TI-Office@users.noreply.github.com>
2023-03-28 16:59:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,28 +15,39 @@ function pararContagem() {
 */
 
 // ---------CONTAGEM REGRESSIVA ----------
-function ativarContagem(){
-    tempo = setInterval(function(){
-         var cronometro = document.getElementById('tempo').value;
-         var soma = parseInt(cronometro) - 1;
-         if (soma === 0){
-            document.getElementById('tempo').value = "tempo esgotado";  
-             
-        pararContagem()
-         
-         } else {
 
-         document.getElementById('tempo').value = soma;
-         }
-        }, 1000);
-     }
- 
- function pararContagem() {
-     clearInterval(tempo);
-     //document.getElementById('tempo').innerHTML = " parou a contagem";
-     }
 
-     function limpar() {
-        document.getElementById("tempo").value = "";
-        clearInterval(tempo);
+
+function ativarContagem() {
+    tempo = setInterval(function () {
+        var cronometro = document.getElementById('tempo').value;
+        var soma = parseInt(cronometro) - 1;
+
+       
+        if (soma <= 0) {
+            document.getElementById('tempo').value = "tempo esgotado";
+            pararContagem()
+
+        } else {
+
+            document.getElementById('tempo').value = soma;
+        }
     }
+    
+    
+    , 1000);
+    
+
+    
+}
+
+function pararContagem() {
+    clearInterval(tempo);
+    return
+    //document.getElementById('tempo').innerHTML = " parou a contagem";
+}
+
+function limpar() {
+    document.getElementById("tempo").value = "";
+    clearInterval(tempo);
+}
